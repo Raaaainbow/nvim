7d35cf2c64e6f5763e9317afc9ffbf5f5e83a1ca
Raaaainbow <sebastian@tjr.dk>
2025-02-15 11:09:22 +0100

    block cursor for all modes added

--- lua/taylor/keymaps.lua
+++ lua/taylor/keymaps.lua
@@ -5,6 +5,7 @@ vim.opt.relativenumber = true
 vim.opt.scrolloff = 8
 vim.opt.undofile = true -- long undo history
 vim.opt.updatetime = 50 -- faster update time
+vim.opt.guicursor = "n-v-i-c:block-Cursor" -- sets block cursor for all modes
 -- for better tab spacing
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
