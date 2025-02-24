7f31f33ad111c7d71d3bad195c0e2f32d203a0b0
Raaaainbow <sebastian@tjr.dk>
2025-02-24 13:04:57 +0100

    undo location updated

--- lua/taylor/keymaps.lua
+++ lua/taylor/keymaps.lua
@@ -4,6 +4,7 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 8
 vim.opt.undofile = true -- long undo history
+vim.opt.undodir = '~/.config/nvim/.undo//'
 vim.opt.updatetime = 50 -- faster update time
 vim.opt.guicursor = "n-v-i-c:block-Cursor" -- sets block cursor for all modes
 -- for better tab spacing
@@ -15,8 +16,8 @@ vim.keymap.set('v', '<leader>y', '"+y', {silent = true})
 vim.keymap.set('v', '<leader>p', '"+p', {silent = true})
 vim.keymap.set('n', '<leader>p', '"+p', {silent = true})
 -- for start and end of line
-vim.keymap.set('n', '<leader>h', '^', {silent = true})
-vim.keymap.set('n', '<leader>l', '$', {silent = true})
+-- vim.keymap.set('n', '<leader>h', '^', {silent = true})
+-- vim.keymap.set('n', '<leader>l', '$', {silent = true})
 -- General keymaps
 -- vim.keymap.set('n', '<leader>e', ':Explore<cr>', {silent = true})
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
